466435d11eb4d3ca830e516c614c77767f77fa6e
ndennyy <ndenny@iastate.edu>
2022-02-12 14:48:06 -0600

    Added 4417_EditedData.csv

--- OnyxCode/Intermediate_Value.py
+++ OnyxCode/Intermediate_Value.py
@@ -17,5 +17,7 @@ eff_32 = 3**(3/4) / (4*c_d_0**(1/4)*k**(3/4))  # 3/2 Efficiency Ratio
 eff = math.sqrt(1 / (4*k*c_d_0))  # Cl/Cd Efficiency Ratio
 eff_12 = 0.75 * (1 / 3*k*(c_d_0**3))**(1/4)  # 1/2 Efficiency Ratio
 
-v_max_range = math.sqrt((2*(m_takeoff*g) / (rho_0*S_to) * math.sqrt(k / c_d_0)))
+v_max_end = math.sqrt((2*(m_takeoff*g) / (rho_0*S_to) * math.sqrt(k / c_d_0)))
+
+v_max_range = math.sqrt((2*(m_takeoff*g) / (rho_0*S_to) * math.sqrt((3*k) / c_d_0)))
 
