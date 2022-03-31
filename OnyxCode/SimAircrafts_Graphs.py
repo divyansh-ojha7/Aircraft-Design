6f3a862c94df7c7541444ed972cc2b489d65f67b
ndennyy <ndenny@iastate.edu>
2022-03-31 14:15:06 -0500

    Made MTOGW vs RC graph look better

--- OnyxCode/SimAircrafts_Graphs.py
+++ OnyxCode/SimAircrafts_Graphs.py
@@ -43,11 +43,12 @@ x = range(-400000, 1000000)
 y = [a*x + b for x in x]
 
 plt.plot(x, y, color='red')
-plt.title('MTOW vs Aspect Ratio')
+plt.title('MTOGW vs Aspect Ratio')
 plt.ylabel('Aspect Ratio')
-plt.xlabel('MTOW (lbs)')
+plt.xlabel('MTOGW [lbs]')
 plt.xlim(-400000,1000000)
 plt.xticks([0, 200000, 400000, 600000, 800000, 1000000])
+plt.grid()
 plt.show()
 
 
@@ -80,10 +81,12 @@ plt.plot(GS2T[0], GS2T[1], marker='o', color='black')
 plt.annotate('Grumman S2T Airtanker', GS2T, textcoords='offset points', xytext=(0,-15), ha='center')
 plt.xlim(-10,160)
 plt.ylim(-200000, 800000)
-plt.title('W/S vs MTOW')
+plt.title('W/S vs MTOGW')
 plt.xlabel('W/S [lb/sqft]')
-plt.ylabel('MTOW [lb]')
+plt.ylabel('MTOGW [lb]')
 plt.yticks([0, 200000, 400000, 600000, 800000])
+plt.xticks([0, 40, 80, 120, 160])
+plt.grid()
 plt.show()
 
 # Format is [MTOW, Retardant Capacity [Gallons]]
@@ -102,30 +105,33 @@ y = [BAE_146[1], LHC130[1], CL215[1], DC10[1], B747[1], GS2T[1]]
 
 
 plt.plot(BAE_146[0], BAE_146[1], marker='o', color='black')
-plt.annotate('British Aerospace 146', BAE_146, textcoords='offset points', xytext=(-10,-15), ha='center')
+plt.annotate('British Aerospace 146', BAE_146, textcoords='offset points', xytext=(-30, 10), ha='center')
 
 
 plt.plot(LHC130[0], LHC130[1], marker='o', color='black')
-plt.annotate('Lockheed HC-130', LHC130, textcoords='offset points', xytext=(0,-15), ha='center')
+plt.annotate('Lockheed HC-130', LHC130, textcoords='offset points', xytext=(45, -3), ha='center')
 
 plt.plot(CL215[0], CL215[1], marker='o', color='black')
-plt.annotate('Canadair CL-215', CL215, textcoords='offset points', xytext=(0,-15), ha='center')
+plt.annotate('Canadair CL-215', CL215, textcoords='offset points', xytext=(-45, 0), ha='center')
 
 plt.plot(DC10[0], DC10[1], marker='o', color='black')
-plt.annotate('Douglas DC-10', DC10, textcoords='offset points', xytext=(0,10), ha='center')
+plt.annotate('Douglas DC-10', DC10, textcoords='offset points', xytext=(-40, 2), ha='center')
 
 plt.plot(B747[0], B747[1], marker='o', color='black')
-plt.annotate('Boeing 747', B747, textcoords='offset points', xytext=(0,10), ha='center')
+plt.annotate('Boeing 747', B747, textcoords='offset points', xytext=(-30, 2), ha='center')
 
 plt.plot(GS2T[0], GS2T[1], marker='o', color='black')
-plt.annotate('Grumman S2T Airtanker', GS2T, textcoords='offset points', xytext=(0,-15), ha='center')
+plt.annotate('Grumman S2T Airtanker', GS2T, textcoords='offset points', xytext=(50,-12), ha='center')
 
 x = range(-400000, 1000000)
 y = [a*x + b for x in x]
 plt.plot(x, y, color='red')
-plt.xlim(0, 800000)
-plt.ylim(0, 25000)
-plt.title('MTOW [lb] vs Retardant Capacity [Gallons]')
-plt.xlabel('MTOW [lb]')
-plt.ylabel('Retardant Capacity [Gallons]')
+plt.xlim(-300000, 800000)
+plt.ylim(-5000, 25000)
+plt.xticks([0, 200000, 400000, 600000, 800000])
+plt.yticks([0, 5000, 10000, 15000, 20000, 25000])
+plt.grid()
+plt.title('MTOGW vs Retardant Capacity')
+plt.xlabel('MTOGW [lb]')
+plt.ylabel('Retardant Capacity [gal]')
 plt.show()
