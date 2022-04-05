9324f98efd7cacc708c990b0cc1f82d710cb6146
ndennyy <ndenny@iastate.edu>
2022-04-04 20:37:05 -0500

    Empty Weight vs MTOGW

--- OnyxCode/SimAircrafts_Graphs.py
+++ OnyxCode/SimAircrafts_Graphs.py
@@ -135,3 +135,54 @@ plt.title('MTOGW vs Retardant Capacity')
 plt.xlabel('MTOGW [lb]')
 plt.ylabel('Retardant Capacity [gal]')
 plt.show()
+
+
+
+# Format is [Empty Weight, MTOGW lb]
+
+BAE_146 = [52510, 84000] # British Aerospace 146
+LHC130 = [76700, 175000] # Lockheed HC-130
+CL215 = [26808, 43499] # Canadair CL-215
+DC10 = [240171, 430000] # Douglas DC-10
+B747 = [412300, 735000] # Boeing 747
+GS2T = [18316, 26147] # Grumman S2T Airtanker
+
+# Line of Best Fit
+x = [BAE_146[0], LHC130[0], CL215[0], DC10[0], B747[0], GS2T[0]]
+y = [BAE_146[1], LHC130[1], CL215[1], DC10[1], B747[1], GS2T[1]]
+[a, b] = np.polyfit(x, y, 1)
+
+
+plt.plot(BAE_146[0], BAE_146[1], marker='o', color='black')
+plt.annotate('British Aerospace 146', BAE_146, textcoords='offset points', xytext=(50, -5), ha='center')
+
+
+plt.plot(LHC130[0], LHC130[1], marker='o', color='black')
+plt.annotate('Lockheed HC-130', LHC130, textcoords='offset points', xytext=(-40, 3), ha='center')
+
+plt.plot(CL215[0], CL215[1], marker='o', color='black')
+plt.annotate('Canadair CL-215', CL215, textcoords='offset points', xytext=(-45, -5), ha='center')
+
+plt.plot(DC10[0], DC10[1], marker='o', color='black')
+plt.annotate('Douglas DC-10', DC10, textcoords='offset points', xytext=(-40, 2), ha='center')
+
+plt.plot(B747[0], B747[1], marker='o', color='black')
+plt.annotate('Boeing 747', B747, textcoords='offset points', xytext=(-30, 2), ha='center')
+
+plt.plot(GS2T[0], GS2T[1], marker='o', color='black')
+plt.annotate('Grumman S2T Airtanker', GS2T, textcoords='offset points', xytext=(50,-12), ha='center')
+
+x = range(-400000, 1000000)
+y = [a*x + b for x in x]
+plt.plot(x, y, color='red')
+
+
+plt.grid()
+plt.xlim([-400000, 700000])
+plt.ylim([-300000, 1000000])
+plt.xticks([0, 200000, 400000, 600000])
+plt.yticks([0, 200000, 400000, 600000, 800000, 1000000])
+plt.title('Empty Weight vs. MTOGW')
+plt.xlabel('Empty Weight [lb]')
+plt.ylabel('MTOGW [lb]')
+plt.show()
